7eedeac7b4b8d2a89006a44a8ab6c17a47c7af4f
suwarnpant <74554755+suwarnpant@users.noreply.github.com>
2025-11-29 23:12:29 +0530

    Update 0_home.py

--- pages/0_home.py
+++ pages/0_home.py
@@ -18,7 +18,7 @@ def get_unsplash_image():
     try:
         url = (
             "https://api.unsplash.com/photos/random?"
-            "query=black,space&orientation=landscape&client_id="
+            "query=dark space&orientation=landscape&client_id="
             + UNSPLASH_API_KEY
         )
 
