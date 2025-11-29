3faa07f8baaba01d799c61ad751bdc5ad44161c6
suwarnpant <74554755+suwarnpant@users.noreply.github.com>
2025-11-29 23:15:11 +0530

    Update 0_home.py

--- pages/0_home.py
+++ pages/0_home.py
@@ -17,7 +17,7 @@ UNSPLASH_API_KEY = st.secrets["unsplash"]["api_key"]
 def get_unsplash_image():
     try:
         url = (
-            "https://api.unsplash.com/photos/random?"
+            "https://api.unsplash.com/search/photos"
             "query=dark space&orientation=landscape&client_id="
             + UNSPLASH_API_KEY
         )
